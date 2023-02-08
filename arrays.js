b878bbc43ef3895f2430b21bcf3a2ca4733460a9
Levy Stevam <stevamlevy@gmail.com>
2023-02-08 19:53:20 -0300

    primeiro commit manipulação arrays

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
-const nomes = ["Maria", "Joao", "Vitor"];
-const x = nomes.pop();
+// remover elementos do final do array
+let nomes = ["Maria", "Joao", "Vitor"];
+const remove = nomes.pop();
+console.log(nomes, remove);
+// adicionar no final do array
+nomes.push("Larissa");
 console.log(nomes);
+// tirando elementos do array de uma ponta a outra
+let novo = nomes.slice(1,-1);
+console.log(novo);
+// transformando string em array
+let name = "Levy Stevam Queiroz"
+const levy = name.split(" ");
+console.log(levy)
+//array em string
+const dividido = ["levy", "stevam", "queiroz"];
+let g = dividido.join(" ");
+console.log(g)
