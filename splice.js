7aca0f760af5dca5b2aefdedaadced3e03d16390
Levy Stevam <stevamlevy@gmail.com>
2023-02-09 21:35:28 -0300

    add splice.js

---
+++ splice.js
@@ -0,0 +1,6 @@
+//               -6         -5        -4        -3        -2        -1 
+//                0        1          2          3         4         5
+const nomes = ["Kayke", "Larissa", "Patolina", "Careca", "Zezo", "Queixada"];
+const removido = nomes.splice(3, 2, "Armando", "Junior");
+console.log(nomes);
+console.log(removido);
