51447832b7f2a57ac7a81b57b454429b0874563c
Levy Stevam <stevamlevy@gmail.com>
2023-02-10 14:38:22 -0300

    filter js e concatenar arrays

---
+++ filter.js
@@ -0,0 +1,2 @@
+const nums = [5,6,7,12,25,3,78,91,1,7,15,10,11];
+const filnum = nums.filter();
--- splice.js
+++ splice.js
@@ -1,6 +1,13 @@
 //               -6         -5        -4        -3        -2        -1 
 //                0        1          2          3         4         5
-const nomes = ["Kayke", "Larissa", "Patolina", "Careca", "Zezo", "Queixada"];
-const removido = nomes.splice(3, 2, "Armando", "Junior");
-console.log(nomes);
-console.log(removido);
+//const nomes = ["Kayke", "Larissa", "Patolina", "Careca", "Zezo", "Queixada"];
+//const removido = nomes.splice(3, 2, "Armando", "Junior");
+//console.log(nomes);
+//console.log(removido);
+
+//// concatenar arrays
+
+const nums1 = [1,2,3];
+const nums2 = [4,5,6];
+const nums3 = [...nums1,...nums2];
+console.log(`arrays concatenados: ${nums3}`);
