0f21dad75144f37a963c7e5284f4824f3d3089b9
Levy Stevam <stevamlevy@gmail.com>
2023-02-15 12:46:03 -0300

    ajax

---
+++ ajax/assets/js/main.js
@@ -0,0 +1,87 @@
+// const request = obj => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+
+//     xhr.addEventListener('load', () => {
+//       if(xhr.status >= 200 && xhr.status < 300) {
+//         resolve(xhr.responseText);
+//       } else {
+//         reject(xhr.statusText);
+//       }
+//     });
+//   });
+// };
+
+// document.addEventListener('click', e => {
+//   const el = e.target;
+//   const tag = el.tagName.toLowerCase();
+
+//   if (tag === 'a') {
+//     e.preventDefault();
+//     carregaPagina(el);
+//   }
+// });
+
+// async function carregaPagina(el) {
+//   const href = el.getAttribute('href');
+
+//   const objConfig = {
+//     method: 'GET',
+//     url: href
+//   };
+
+//   try {
+//     const response = await request(objConfig);
+//     carregaResultado(response);
+//   } catch(e) {
+//     console.log(e);
+//   }
+// }
+
+// function carregaResultado(response) {
+//   const resultado = document.querySelector('.resultado');
+//   resultado.innerHTML = response;
+// }
+const request = obj =>{
+  const xhr = new XMLHttpRequest();
+  xhr.open(obj.method, obj.url, true);
+  xhr.send();
+  xhr.addEventListener("load", ()=>{
+    if(xhr.status >= 200 && xhr.status <300){
+      obj.sucess(xhr.responseText);
+    }else{
+      obj.erro(xhr.statusText);
+    }
+
+  })
+}
+
+document.addEventListener("click", e =>{
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+  if ( tag === "a"){
+    e.preventDefault();
+    carregaPagina(el);
+  }
+})
+
+function carregaPagina(el){
+  const href = el.getAttribute("href");
+  request({
+    method : "GET",
+    url: href,
+    sucess(response){
+      loadResult(response)
+    },
+    erro(errortext){
+      console.log(errortext)
+    }
+  });
+}
+
+function loadResult(response){
+  const resultado = document.querySelector(".resultado");
+  resultado.innerHTML = response;
+}
