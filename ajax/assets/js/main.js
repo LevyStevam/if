cfa8134ef423cfacfd38bb7b615d4c7804d9e7bc
Levy Stevam <stevamlevy@gmail.com>
2023-02-15 17:38:18 -0300

    ajax 2

--- ajax/assets/js/main.js
+++ ajax/assets/js/main.js
@@ -44,44 +44,82 @@
 //   const resultado = document.querySelector('.resultado');
 //   resultado.innerHTML = response;
 // }
-const request = obj =>{
-  const xhr = new XMLHttpRequest();
-  xhr.open(obj.method, obj.url, true);
-  xhr.send();
-  xhr.addEventListener("load", ()=>{
-    if(xhr.status >= 200 && xhr.status <300){
-      obj.sucess(xhr.responseText);
-    }else{
-      obj.erro(xhr.statusText);
-    }
+// const request = obj =>{
+//   const xhr = new XMLHttpRequest();
+//   xhr.open(obj.method, obj.url, true);
+//   xhr.send();
+//   xhr.addEventListener("load", ()=>{
+//     if(xhr.status >= 200 && xhr.status <300){
+//       obj.sucess(xhr.responseText);
+//     }else{
+//       obj.erro(xhr.statusText);
+//     }
 
-  })
-}
+//   })
+// }
+
+// // document.addEventListener("click", e =>{
+// //   const el = e.target;
+// //   const tag = el.tagName.toLowerCase();
+// //   if ( tag === "a"){
+// //     e.preventDefault();
+// //     carregaPagina(el);
+// //   }
+// // })
+
+// // function carregaPagina(el){
+// //   const href = el.getAttribute("href");
+// //   request({
+// //     method : "GET",
+// //     url: href,
+// //     sucess(response){
+// //       loadResult(response)
+// //     },
+// //     erro(errortext){
+// //       console.log(errortext)
+// //     }
+// //   });
+// // }
+
+// // function loadResult(response){
+// //   const resultado = document.querySelector(".resultado");
+// //   resultado.innerHTML = response;
+// // }
+
+// fetch("pagina4.html")
+//   .then(resposta =>{
+//     if(resposta.status !== 200) throw new Error("erro 404 do teu pai");
+//     return resposta.text();
+//   })
+//   .then (html=> console.log(html))
+//   .catch(e => console.error(e));
 
 document.addEventListener("click", e =>{
   const el = e.target;
   const tag = el.tagName.toLowerCase();
-  if ( tag === "a"){
+  if(tag === "a"){
     e.preventDefault();
     carregaPagina(el);
   }
 })
 
-function carregaPagina(el){
-  const href = el.getAttribute("href");
-  request({
-    method : "GET",
-    url: href,
-    sucess(response){
-      loadResult(response)
-    },
-    erro(errortext){
-      console.log(errortext)
-    }
-  });
-}
+async function carregaPagina(el) {
+  const href = el.getAttribute('href');
+  const response = await fetch(href);
+  if (response.status !== 200) throw new Error("erro do teu pai");
+  const html = await response.text();
+  carregaResultado(html)
+  // fetch(href)
+  //   .then(response => {
+  //     if (response.status !== 200) throw new Error("erro do teu pai");
+  //     return response.text();
+  //   })
+  //   .then(html => carregaResultado(html))
+  //   .catch(e => console.log(e));
+}  
 
-function loadResult(response){
-  const resultado = document.querySelector(".resultado");
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
   resultado.innerHTML = response;
 }
