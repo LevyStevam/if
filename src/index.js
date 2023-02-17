28e2bc4c9f4c73148337949e022e74701d50090d
Levy Stevam <stevamlevy@gmail.com>
2023-02-17 16:53:18 -0300

    começo de modulos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-// 1
-// 2
-// 3
-console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+const nome = "levy";
+const sob = "stevam"
+const idade = 19;
+soma = (x,y) =>{
+    return console.log(x + y);
+}
+soma(5,6);
