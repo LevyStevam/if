651feacf7c397f9f040e0f733f34b48f5a98d6be
Levy Stevam <stevamlevy@gmail.com>
2023-02-22 14:57:26 -0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-const nome = "levy";
-const sob = "stevam"
-const idade = 19;
-soma = (x,y) =>{
-    return console.log(x + y);
-}
-soma(5,6);
+import { nome } from "./mod1";
+console.log(nome)
---
+++ src/mod1.js
@@ -0,0 +1,8 @@
+const nome = "TaoObservando";
+const sob = "NoisMiranda";
+const  idade = 27;
+function soma(x,y){
+    return x + y;
+}
+
+export {nome};
